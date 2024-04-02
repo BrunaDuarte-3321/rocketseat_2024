7c6a51ca69070ec4aebcfb6f6e1cbafa962951d8
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2024-04-01 21:38:22 -0300

    feat: criando repositorio para projetos rocketseat 2024

---
+++ nlw_unite/src/components/header.tsx
@@ -0,0 +1,16 @@
+import nlwUniteIcon from '../assets/logo.svg'
+export function Header() {
+  return (
+    <div className="flex items-center gap-5 py-2">
+      <img src={nlwUniteIcon} alt="" />
+      <nav className="flex items-center gap-5">
+        <a className="font-medium text-sm text-zinc-300" href="">
+          Eventos
+        </a>
+        <a className="font-medium text-sm" href="">
+          Participantes
+        </a>
+      </nav>
+    </div>
+  )
+}
